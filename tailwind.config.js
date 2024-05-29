28c51c977f00570f15284d70a70f739f38a86fd6
Fernand197 <tatchumf@gmail.com>
2024-05-29 18:21:04 +0100

    Fix some ui bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ export default {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'dark': "url('./src/assets/images/bgtwo.jpg')",
-        'light': "url('./src/assets/images/bg.jpg')",
+        'dark': "url('src/assets/images/bgtwo.jpg')",
+        'light': "url('src/assets/images/bg.jpg')",
       },
     },
   },
